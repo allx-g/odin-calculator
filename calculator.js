4216aab34ca8cb3e005755dfbb4064de94778f6c
Alex <alexg125125@gmail.com>
2023-08-05 23:59:01 -0500

    Add variable operation function
    The function takes in two numbers and an operator and determines the
    appropriate operation to evaluate.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// A calculator operation has two operands and an operator
+let operand1;
+let operand2;
+let operator;
+
+function operate(operand1, operator, operand2) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(operand1, operand2);
+            break;
+        case '-':
+            result = subtract(operand1, operand2);
+            break;
+        case '*':
+            result = multiply(operand1, operand2);
+            break;
+        case '/':
+            result = divide(operand1, operand2);
+            break;
+    }
+    return result;
+}
