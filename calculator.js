b389b78b70976cfcdb01d24f671754d544cbe7d7
Alex <alexg125125@gmail.com>
2023-08-07 15:37:37 -0500

    Store button input into variables
    Everytime an operator is pressed, the input number is saved and the
    operand is switched to the next one to allow for input of second value.

--- calculator.js
+++ calculator.js
@@ -14,10 +14,6 @@ function divide(a, b) {
     return a / b;
 }
 
-// A calculator operation has two operands and an operator
-let operand1;
-let operand2;
-let operator;
 
 function operate(operand1, operator, operand2) {
     let result;
@@ -25,26 +21,50 @@ function operate(operand1, operator, operand2) {
         case '+':
             result = add(operand1, operand2);
             break;
-        case '-':
-            result = subtract(operand1, operand2);
-            break;
-        case '*':
+            case '-':
+                result = subtract(operand1, operand2);
+                break;
+                case '*':
             result = multiply(operand1, operand2);
             break;
-        case '/':
-            result = divide(operand1, operand2);
-            break;
-    }
-    return result;
-}
-
-function getInput(e) {
+            case '/':
+                result = divide(operand1, operand2);
+                break;
+            }
+            return result;
+        }
+        
+function manageCalculation(e) {
     const button = e.target;
-    calculatorDisplay.textContent = button.getAttribute('data-key');
+    currentInput = button.getAttribute('data-key');
+    console.log("input: ", currentInput);
+
+
+    if (+currentInput === +currentInput) {
+        switch (currentOperand) {
+            case 1:
+                operand1 += currentInput;
+                console.log("op1: ", operand1);
+                break;
+            case 2:
+                operand2 += currentInput;
+                console.log("op2: ", operand2);
+                break;
+        }
+    }
+    if (currentInput === "add") {
+        currentOperand = (currentOperand == 1) ? 2 : 1; 
+        console.log(currentOperand);
+    }
 }
 
 const calculatorDisplay = document.querySelector("#display-text");
-
 const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener('click', manageCalculation));
 
-buttons.forEach(button => button.addEventListener('click', getInput));
+let currentInput;
+// A calculator operation has two operands and an operator
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let currentOperand = 1;
